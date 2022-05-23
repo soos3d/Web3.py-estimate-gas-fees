708213779be392f9814c4ccefbda5c0104cd0ead
Soos3D <99700157+soos3d@users.noreply.github.com>
2022-05-23 10:01:04 -0300

    Update estimate_gas.py

--- estimate_gas.py
+++ estimate_gas.py
@@ -1,6 +1,6 @@
 from web3 import Web3
 
-node_url = "https://eth-mainnet.alchemyapi.io/v2/5aZT2-5wmHGtbrTRw1brrG3qWYeo9Yhs"
+node_url = "CHAINSTACK NODE URL"
 
 web3 = Web3(Web3.HTTPProvider(node_url))
 
